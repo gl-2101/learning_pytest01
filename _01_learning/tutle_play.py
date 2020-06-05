fa29543e59ce6021984e33460374d8ce2a53e4e8
wake-uppp <805177693@qq.com>
2020-06-05 10:54:30 +0800

    play_code

---
+++ _01_learning/tutle_play.py
@@ -0,0 +1,31 @@
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('red')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.mainloop()
+"""
+        3x - 5  (x > 1)
+f(x) =  x + 2   (-1 <= x <= 1)
+        5x + 3  (x < -1)
+
+Version: 0.1
+Author: 骆昊
+"""
+
+x = float(input('x = '))
+if x > 1:
+    y = 3 * x - 5
+elif x >= -1:
+    y = x + 2
+else:
+    y = 5 * x + 3
+print('f(%.2f) = %.2f' % (x, y))
